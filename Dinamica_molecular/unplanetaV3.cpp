96c87c7f9be47ca1858314f7a93495f5a501488a
luabeltranga <luabeltranga@unal.edu.co>
2017-08-22 11:39:47 -0500

    yuju

---
+++ Dinamica_molecular/unplanetaV3.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cmath>
+#include "Vector.h"
+
+const double GM=1;
+
+class Cuerpo;
+
+
+class Cuerpo{
+private:
+  vector3D r,V,F;double m,R;
+public:
+  void Inicie(double x0,double y0,double z0,double Vx0,double Vy0,double Vz0, double m0,double R0);
+  void CalculeFuerza(void);
+  void Arranque(double dt);
+  void Muevase(double dt);
+  void Dibujese(void);
+  double Getx(void){return r.x();};
+  double Gety(void){return r.y();};
+};
+
+void Cuerpo::Inicie(double x0,double y0,double z0,double Vx0,double Vy0,double Vz0,double m0,double R0){
+  r.cargue(x0,y0,z0);
+  V.cargue(Vx0,Vy0,Vz0);
+  m=m0;
+  R=R0;
+}
+
+void Cuerpo::CalculeFuerza(void){
+  double aux=GM*m*std::pow(norma2(r),-1.5);
+  F=(-aux)*r;
+}
+
+void Cuerpo::Arranque(double dt){
+  V-=F*(0.5*dt/m);
+}
+
+
+void Cuerpo::Muevase(double dt){
+  V+=F*(dt/m);
+  r+=V*dt;
+}
+
+void Cuerpo::Dibujese(void){
+  std::cout<<", "<<r.x()<<"+"<<R<<"*cos(t),"<<r.y()<<"+"<<R<<"*sin(t)";
+}
+//------------------Funciones Globales---------
+
+void InicieAnimacion(void){
+  //std::cout<<"set terminal gif animate"<<std::endl;
+  //std::cout<<"set output 'planeta.gif'"<<std::endl;
+  std::cout<<"unset key"<<std::endl;
+  std::cout<<"set size ratio -1"<<std::endl;
+  std::cout<<"set xrange [-120:120]"<<std::endl;
+  std::cout<<"set yrange [-120:120]"<<std::endl;
+  std::cout<<"set parametric"<<std::endl;
+  std::cout<<"set trange[0:7]"<<std::endl;
+  std::cout<<"set isosamples 12"<<std::endl;
+}
+void InicieCuadro(void){
+  std::cout<<"plot 0,0 ";
+}
+void TermineCuadro(void){
+  std::cout<<std::endl;
+}
+
+int main(void){
+  double t, dt=1.0,tmax;
+  double tdibujo;int Ndibujos;
+  Cuerpo Planeta;
+
+  double r,omega,V,T,m,R;
+  R=5; m=1; r=100; omega=std::sqrt(GM/(r*r*r)); V=omega*r; T=2*M_PI/omega; tmax=1.1*T;  
+
+  
+  //-----(x0, y0, Vx0, Vy0, m0, R0);
+  //InicieAnimacion(); Ndibujos=50;
+  Planeta.Inicie(r, 0, 0, 0, 0.5*V,0, m, R);
+  Planeta.CalculeFuerza();
+  Planeta.Arranque(dt);
+  for(t=tdibujo=0;t<tmax;t+=dt,tdibujo+=dt){
+    /*if(tdibujo<tmax/Ndibujos){
+      InicieCuadro();
+      Planeta.Dibujese();
+      TermineCuadro();
+      tdibujo=0;
+    }
+    */
+    std::cout<<Planeta.Getx()<<"   "<<Planeta.Gety()<<std::endl;
+    Planeta.CalculeFuerza();
+    Planeta.Muevase(dt);
+  }
+  
+  return 0;
+}
+
+
---
+++ Dinamica_molecular/unplanetaV5_ForestRuth.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <cmath>
+#include "Vector.h"
+
+const double GM=1;
+const double THETA=1.0/(2-std::cbrt(2.0));
+class Cuerpo;
+
+
+class Cuerpo{
+private:
+  vector3D r,V,F;double m,R;
+public:
+  void Inicie(double x0,double y0,double z0,double Vx0,double Vy0,double Vz0, double m0,double R0);
+  void CalculeFuerza(void);
+  void Mueva_r(double dt,double Constante);
+  void Mueva_V(double dt,double Constante);
+  void Dibujese(void);
+  double Getx(void){return r.x();};
+  double Gety(void){return r.y();};
+};
+
+void Cuerpo::Inicie(double x0,double y0,double z0,double Vx0,double Vy0,double Vz0,double m0,double R0){
+  r.cargue(x0,y0,z0);
+  V.cargue(Vx0,Vy0,Vz0);
+  m=m0;
+  R=R0;
+}
+
+void Cuerpo::CalculeFuerza(void){
+  double aux=GM*m*std::pow(norma2(r),-1.5);
+  F=(-aux)*r;
+}
+
+void Cuerpo::Mueva_r(double dt,double Constante){
+r+=V*(Constante*dt);
+}
+void Cuerpo::Mueva_V(double dt,double Constante){
+V+=F*(Constante*dt);
+}
+
+void Cuerpo::Dibujese(void){
+  std::cout<<", "<<r.x()<<"+"<<R<<"*cos(t),"<<r.y()<<"+"<<R<<"*sin(t)";
+}
+//------------------Funciones Globales---------
+
+void InicieAnimacion(void){
+  //std::cout<<"set terminal gif animate"<<std::endl;
+  //std::cout<<"set output 'planeta.gif'"<<std::endl;
+  std::cout<<"unset key"<<std::endl;
+  std::cout<<"set size ratio -1"<<std::endl;
+  std::cout<<"set xrange [-120:120]"<<std::endl;
+  std::cout<<"set yrange [-120:120]"<<std::endl;
+  std::cout<<"set parametric"<<std::endl;
+  std::cout<<"set trange[0:7]"<<std::endl;
+  std::cout<<"set isosamples 12"<<std::endl;
+}
+void InicieCuadro(void){
+  std::cout<<"plot 0,0 ";
+}
+void TermineCuadro(void){
+  std::cout<<std::endl;
+}
+
+int main(void){
+  double t, dt=1.0,tmax;
+  double tdibujo;int Ndibujos;
+  Cuerpo Planeta;
+
+  double r,omega,V,T,m,R;
+  R=5; m=1; r=100; omega=std::sqrt(GM/(r*r*r)); V=omega*r; T=2*M_PI/omega; tmax=1.1*T;  
+
+  
+  //-----(x0, y0, Vx0, Vy0, m0, R0);
+  //InicieAnimacion(); Ndibujos=50;
+  Planeta.Inicie(r, 0, 0, 0, 0.5*V,0, m, R);
+  Planeta.CalculeFuerza();
+  for(t=tdibujo=0;t<tmax;t+=dt,tdibujo+=dt){
+    /*if(tdibujo<tmax/Ndibujos){
+      InicieCuadro();
+      Planeta.Dibujese();
+      TermineCuadro();
+      tdibujo=0;
+    }
+    */
+    std::cout<<Planeta.Getx()<<"   "<<Planeta.Gety()<<std::endl;
+    Planeta.Mueva_r(dt,THETA/2.0);
+    Planeta.CalculeFuerza();
+    Planeta.Mueva_V(dt,THETA);
+    Planeta.Mueva_r(dt,(1-THETA)/2.0);
+    Planeta.CalculeFuerza();
+    Planeta.Mueva_V(dt,1-2*THETA);
+    Planeta.Mueva_r(dt,(1-THETA)/2.0);
+    Planeta.CalculeFuerza();
+    Planeta.Mueva_V(dt,THETA);
+    Planeta.Mueva_r(dt,THETA/2.0);
+  }
+  
+  return 0;
+}
+
+
